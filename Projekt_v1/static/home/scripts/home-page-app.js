17ed38a87a00bf92fc24b080f8ebf4c6e0dfa38a
jacob0202 <kubsoch23@gmail.com>
2022-11-09 18:53:49 +0100

    Folders and files structure changed

--- Projekt_v1/static/home/scripts/home-page-app.js
+++
@@ -1,8 +0,0 @@
-const expandArrow = document.querySelector('#expandArrow');
-const dropDown = document.querySelector('.dropDown');
-const inputWrapper = document.querySelector('.inputWrapper');
-const searchIcon = document.querySelector('.searchIcon')
-expandArrow.addEventListener('click', () => {
-    inputWrapper.classList.toggle('inputWrapperWider');
-    dropDown.classList.toggle('displayFlex');
-})
